0d5a58703c7fc8cc4106060bda74c47c728d1c2b
Joe Concha Vasquez <joeconchavasquez@gmail.com>
2024-05-22 13:47:34 -0500

    Finished implementation of the face detection box

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+//import Signin from "./components/Signin/Signin";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
 import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
-import NoImage from "./components/NoImage/NoImage";
 import ParticlesBg from "particles-bg";
 import "./App.css";
 
 function App() {
   const [searchBoxChange, setSearchBoxChange] = useState("");
+  const [box, setBox] = useState({});
+
+  /*
+   leftCol: 0,
+    topRow: 0,
+    rightCol: 0,
+    bottomRow: 0,
+  */
 
   const onSearchChange = (event) => {
     //console.log("this is the event: ", event.target.value);
     setSearchBoxChange(event.target.value);
     console.log("Tiene texto: ", event.target.value);
+  };
+
+  const calculateFaceLocation = (data) => {
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputimage");
+    const width = Number(image.width);
+    const height = Number(image.height);
+    console.log(width, height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height,
+    };
+  };
+
+  const displayBox = (box) => {
+    console.log("Box object: ", box);
+    setBox(box);
   };
 
   const setUpAPI = (imgURL) => {
@@ -24,7 +52,7 @@ function App() {
     const USER_ID = "joecv10";
     const APP_ID = "test";
     // Change these to whatever model and image URL you want to use
-    const MODEL_ID = "face-detection";
+    //const MODEL_ID = "face-detection";
     const IMAGE_URL = imgURL;
 
     const raw = JSON.stringify({
@@ -62,11 +90,15 @@ function App() {
       setUpAPI(searchBoxChange)
     )
       .then((response) => response.json())
-      .then((result) => console.log(result))
+      .then((result) => {
+        console.log("This is the result: ", result);
+        displayBox(calculateFaceLocation(result));
+      })
       .catch((error) => console.log("error", error));
   };
   return (
     <>
+      {/* {<Signin />} */}
       <ParticlesBg type="cobweb" bg={true} />
       <Navigation />
       <Logo />
@@ -75,7 +107,7 @@ function App() {
         onSearchChange={onSearchChange}
         onButtonSubmit={onButtonSubmit}
       />
-      <FaceRecognition imgURL={searchBoxChange} />
+      <FaceRecognition box={box} imgURL={searchBoxChange} />
     </>
   );
 }
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,16 +1,39 @@
 import React from "react";
+import "./FaceRecognition.css";
 
-const FaceRecognition = ({ imgURL }) => {
+const FaceRecognition = ({ imgURL, box }) => {
   return (
     <>
       <div className="center ma">
         <div className="absolute mt5 mb5">
           <img
+            id="inputimage"
             src={imgURL}
-            alt="Recognition item"
+            alt=""
             width="500px"
             height="auto"
           />
+          {Object.keys(box).length > 0 && (
+            <div
+              className="bounding-box"
+              style={{
+                top: box.topRow,
+                right: box.rightCol,
+                bottom: box.bottomRow,
+                left: box.leftCol,
+              }}
+            ></div>
+          )}
+
+          {/* <div
+            className="bounding-box"
+            style={{
+              top: box.topRow,
+              right: box.rightCol,
+              bottom: box.bottomRow,
+              left: box.leftCol,
+            }}
+          ></div>*/}
         </div>
       </div>
     </>
